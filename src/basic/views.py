0bba6b968980d3c2255d9eb5014195a7ececba92
valsorym <valsorym.e@gmail.com>
2015-12-19 13:03:17 +0200

    Integrate main page use the page manager (for further work is needed final template).

--- src/basic/views.py
+++ src/basic/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.views.generic import FormView
 from django.core.urlresolvers import reverse_lazy
 
 from audience.forms import FeedbackForm
+from content.models import Page
 
 
 class HomeView(FormView):
@@ -9,6 +11,15 @@ class HomeView(FormView):
     template_name = 'this/home.jinja'
     form_class = FeedbackForm
     success_url = reverse_lazy('home')
+
+    def get_context_data(self, *args, **kwargs):
+        """Get context data."""
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+
+        query = Q(is_active=True)
+        pages = Page.objects.filter(query)
+        context['pages'] = {obj.role:obj for obj in pages}
+        return context
 
     def form_valid(self, form):
         """Save message and inform all subscribers."""
